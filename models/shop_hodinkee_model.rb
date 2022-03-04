529d3a1f5c4e6ec82e623c5c5cb7b96b011d1ac1
OOrlova <alexandra.volosko@gmail.com>
2022-03-04 10:48:08 +0200

    Fix comments

--- models/shop_hodinkee_model.rb
+++ models/shop_hodinkee_model.rb
@@ -8,6 +8,5 @@ module Models
     def initialize(**args)
       super
     end
-
   end
 end
---
+++ services/parsers/parser.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require 'nokogiri'
+
+module Parsers
+  class Parser
+    def initialize(html)
+      @html = html
+    end
+
+    def product_links(tag)
+      Nokogiri::HTML(@html).search(tag).map { |link| link[:href] }
+    end
+
+    def parse_link(tag)
+      product_links(tag)[0]
+    end
+
+    def parse_content_by_tag(tag)
+      Nokogiri::HTML(@html).search(tag).map(&:content)
+    end
+  end
+end
